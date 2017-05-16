8deb4c77f6b91f8ae2c7d53de6e697646db085c4
Takahisa Kurimura <takahisa.kurimura@eure.jp>
2017-05-16 17:18:29 +0900

    fix typo

--- src/clock.rb
+++ src/clock.rb
@@ -3,6 +3,6 @@ require_relative "access"
 require 'clockwork'
 include Clockwork
 
-every(1.hour, 'job', at: ['10:00', '19:00']) 
+every(1.hour, 'job', at: ['10:00', '19:00']) do
    `bundle exec ruby src/access.rb`
 end
